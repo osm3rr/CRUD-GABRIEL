6f9b1d40f494846e815b6f99289d50bc93f87f2b
osm3rr <osm3rr@adakademy.com>
2024-07-06 18:30:32 -0400

    agregamos iamgenes

--- app1/models.py
+++ app1/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Publicaciones(models.Model):
     titulo = models.CharField(max_length = 50)
     descripcion = models.TextField()
-    author = models.ForeignKey("auth.User", on_delete = models.CASCADE, blank = True)
+    author = models.ForeignKey("auth.User", on_delete = models.CASCADE, blank = True,null=True)
+    image= models.ImageField(upload_to="media", blank=True, null=True)
+
     def __str__(self):
         return self.titulo
